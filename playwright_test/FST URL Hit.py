f6dac1716f98779df2bd907ba87df61d3816023f
ayushi.ganu <ayushi.ganu@gmail.com>
2025-04-22 12:36:12 +0530

    Commit for Testing by Ayushi Ganu.

--- playwright_test/FST URL Hit.py
+++ playwright_test/FST URL Hit.py
@@ -5,7 +5,7 @@ from playwright.async_api import async_playwright
 
 async def run():
     async with async_playwright() as p:
-        browser = await p.chromium.launch(headless=False)  # Set to True to run in background
+        browser = await p.chromium.launch(headless=True)  # Set to True to run in background
         page = await browser.new_page()
         await page.goto("http://10.10.1.102:6001/")
         #await page.screenshot(path="example.png")
