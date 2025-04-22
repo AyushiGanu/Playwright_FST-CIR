7d929d6821bb3e2300e478c9e50eaf920043275c
ayushi.ganu <ayushi.ganu@gmail.com>
2025-04-22 10:47:04 +0530

    Commit for Testing by Ayushi Ganu.

---
+++ playwright_test/FST URL Hit.py
@@ -0,0 +1,14 @@
+import asyncio
+import time
+
+from playwright.async_api import async_playwright
+
+async def run():
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=False)  # Set to True to run in background
+        page = await browser.new_page()
+        await page.goto("http://10.10.1.102:6001/")
+        #await page.screenshot(path="example.png")
+        await browser.close()
+        time.sleep (10)
+asyncio.run(run())
