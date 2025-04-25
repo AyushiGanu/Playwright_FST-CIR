9de17bc79a59e9327e76e39babc4b832b18f99c3
ayushi.ganu <ayushi.ganu@systematixindia.com>
2025-04-25 15:45:27 +0530

    Commit for Testing by Ayushi Ganu.

--- playwright_test/CodeGen_Test-FST.py
+++ playwright_test/CodeGen_Test-FST.py
@@ -1,47 +1,89 @@
 import re
 import time
+from pydoc import browse
+
+#import pyautogui
+
+# Get screen size
+#screen_width, screen_height = pyautogui.size()
+
+# Adjust size to prevent overflow
+#adjusted_width = screen_width - 100
+#adjusted_height = screen_height - 100
 
 from playwright.sync_api import Playwright, sync_playwright, expect
 
-
 def run(playwright: Playwright) -> None:
+    # Get screen width and height
+    #screen_width, screen_height = pyautogui.size()
+    #print(f"Screen size: {screen_width}x{screen_height}")
     browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context()
+
+    # Set viewport to match screen resolution
+    context = browser.new_context(no_viewport=True)
+
     page = context.new_page()
     page.goto("http://10.10.1.102:6001/login")
+    print("Waiting for Masters element to appear...")
+
     page.get_by_role("textbox", name="User Name").click()
-    time.sleep(5)
+    print("User Name found")
+    time.sleep(3)
+
     page.get_by_role("textbox", name="User Name").fill("Ayushi")
-    time.sleep(5)
+    print("User name input")
+    time.sleep(3)
+
     page.get_by_role("textbox", name="Password").click()
-    time.sleep(5)
+    print("Password found")
+    time.sleep(3)
+
     page.get_by_role("textbox", name="Password").fill("Cir@123")
-    time.sleep(5)
+    print("Password input")
+    time.sleep(3)
+
     page.get_by_role("button", name="Login").click()
-    time.sleep(5)
+    print("Login Successful")
+    time.sleep(3)
+
     page.get_by_role("link", name="Dashboard icon Dashboard").click()
-    time.sleep(5)
+    print("Dashboard icon Dashboard")
+    time.sleep(3)
+
     page.get_by_role("link", name="Master icon Masters").click()
-    time.sleep(5)
+    print("Master icon Masters")
+    time.sleep(3)
+
     page.get_by_role("link", name="Additional Masters icon").click()
-    time.sleep(5)
+    print("Additional Masters icon")
+    time.sleep(3)
+
     page.get_by_role("link", name="Costing icon Costing").click()
-    time.sleep(5)
+    print("Costing icon Costing")
+    time.sleep(3)
+
     page.get_by_role("link", name="Simulation icon Simulation").click()
-    time.sleep(5)
+    print("Simulation icon Simulation")
+    time.sleep(3)
+
     page.get_by_role("link", name="Reports And Analytics icon").click()
-    time.sleep(5)
+    print("Reports And Analytics icon")
+    time.sleep(3)
+
     page.get_by_role("link", name="Users icon Users").click()
-    time.sleep(5)
+    print("Users icon Users")
+    time.sleep(3)
+
     page.get_by_role("button", name="logout").click()
-    time.sleep(5)
+    print("Logout Successful")
+    time.sleep(3)
+
     page.get_by_role("button", name="OK").click()
-    time.sleep(5)
+    print("OK button")
+    time.sleep(3)
 
     # ---------------------
     context.close()
     browser.close()
-
-
 with sync_playwright() as playwright:
     run(playwright)
